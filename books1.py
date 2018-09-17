e1fe88a84b00cb429f35c05a4ffffee4214512cc
partidaa <36749876+partidaa@users.noreply.github.com>
2018-09-16 19:13:26 -0500

    Delete books1.py

--- books1.py
+++
@@ -1,8 +0,0 @@
-import csv
-import sys
-with open('books.csv', newline='') as f:
-    reader = csv.reader(f)
-    #for row in reader:
-        #print(row)
-    for row in reader:
-        print (sys.argv[1])
