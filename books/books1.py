77eb1de08da762169819c1c0a780a855547d8667
adoseofkellen <32659062+adoseofkellen@users.noreply.github.com>
2018-09-18 19:58:15 -0500

    Updated the python file.

--- books/books1.py
+++ books/books1.py
@@ -1,17 +1,20 @@
+#Addison Partida, Javin White, Kellen Dorchen
+#September 18, 2018
+#Reads a csv file and sorts according to command line argument.
 import csv
 import sys
 
 with open('books.csv', 'r', encoding='utf-8') as csv_file:
     csv_reader = csv.reader(csv_file)
-   
-    
+
+
     if sys.argv[1] == "books":
         books= []
         for line in csv_reader:
             books.append(line[0])
         books.sort()
         print (books)
-    
+
     if sys.argv[1] == "authors":
         authors=[]
         for line in csv_reader:
