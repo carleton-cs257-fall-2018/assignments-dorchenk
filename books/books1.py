0a45a45eac45aa8dd8e4aef3a09829f7c65f86ef
Javin White <whitej2@carleton.edu>
2018-09-19 12:16:51 -0500

    with optional command line

--- books/books1.py
+++ books/books1.py
@@ -6,74 +6,77 @@
 import csv
 import sys
 
+if len(sys.argv) <= 3:
+	print ("Usage: python3 books1.py input-file action [sort-direction]", file=sys.stderr)
 
-with open(sys.argv[1], 'r', encoding='utf-8') as csv_file:
-	csv_reader = csv.reader(csv_file)
+else:
+	with open(sys.argv[1], 'r', encoding='utf-8') as csv_file:
+		csv_reader = csv.reader(csv_file)
 
-	if sys.argv[2] == "books":
-		books = []
-		for line in csv_reader:
-			books.append(line[0])
+		if sys.argv[2] == "books":
+			books = []
+			for line in csv_reader:
+				books.append(line[0])
 
-		if sys.argv[3] == "forward":
-			books.sort()
-			for i in books:
-				print (i)
+				if sys.argv[3] == "forward":
+					books.sort()
+					for i in books:
+						print (i)
 
-		elif sys.argv[3] == "reverse":
-			books.sort(reverse = True)
-			for i in books:
-				print (i)
+				elif sys.argv[3] == "reverse":
+					books.sort(reverse = True)
+					for i in books:
+						print (i)
 
 
-	elif sys.argv[2] == "authors":
-		authors=[]
-		for line in csv_reader:
-			inputstr = line[2]
-			words = inputstr.split()
-			secondwords = iter(words)
-			next(secondwords)
-			if len(words) == 3:
-				tempstr = words[1] + " " + words[0]
-				authors.append(tempstr)
-
-			elif len(words) > 4:
-				tempstr = words[4] + " " + words[5]
-				authors.append(tempstr)
-
-			elif len(words) == 4:
-				tempstr = words[2] + " " + words[0] + " " + words[1]
-				authors.append(tempstr)
-
-		if sys.argv[3] == "forward":
-			authors.sort()
-			forwardlist = []
-			for i in authors:
-				inputstr = i
+		elif sys.argv[2] == "authors":
+			authors=[]
+			for line in csv_reader:
+				inputstr = line[2]
 				words = inputstr.split()
+				secondwords = iter(words)
+				next(secondwords)
 				if len(words) == 3:
-					tempstr = words[1] + " " + words[2] + " " + words[0]
-					forwardlist.append(tempstr)
-				else:
 					tempstr = words[1] + " " + words[0]
-					forwardlist.append(tempstr)
-			for j in forwardlist:
-				print (j)
+					authors.append(tempstr)
 
-		elif sys.argv[3] == "reverse":
-			authors.sort(reverse = True)
-			reverselist = []
-			for i in authors:
-				inputstr = i
-				words = inputstr.split()
-				if len(words) == 3:
-					tempstr = words[1] + " " + words[2] + " " + words[0]
-					reverselist.append(tempstr)
-				else:
-					tempstr = words[1] + " " + words[0]
-					reverselist.append(tempstr)
-			for j in reverselist:
-				print (j)
+				elif len(words) > 4:
+					tempstr = words[4] + " " + words[5]
+					authors.append(tempstr)
 
-	else:
-		print ("Usage: python3 books1.py input-file action [sort-direction]", file=sys.stderr)
+				elif len(words) == 4:
+					tempstr = words[2] + " " + words[0] + " " + words[1]
+					authors.append(tempstr)
+
+			if sys.argv[3] == "forward":
+				authors.sort()
+				forwardlist = []
+				for i in authors:
+					inputstr = i
+					words = inputstr.split()
+					if len(words) == 3:
+						tempstr = words[1] + " " + words[2] + " " + words[0]
+						forwardlist.append(tempstr)
+					else:
+						tempstr = words[1] + " " + words[0]
+						forwardlist.append(tempstr)
+				for j in forwardlist:
+					print (j)
+
+			elif sys.argv[3] == "reverse":
+				authors.sort(reverse = True)
+				reverselist = []
+				for i in authors:
+					inputstr = i
+					words = inputstr.split()
+					if len(words) == 3:
+						tempstr = words[1] + " " + words[2] + " " + words[0]
+						reverselist.append(tempstr)
+					else:
+						tempstr = words[1] + " " + words[0]
+						reverselist.append(tempstr)
+				for j in reverselist:
+					print (j)
+
+	# 	else:
+	# 		print ("Usage: python3 books1.py input-file action [sort-direction]", file=sys.stderr)
