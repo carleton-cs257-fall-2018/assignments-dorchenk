e09338fd7e9d565c8804636737ac8c7d9d8c34e3
partidaa <36749876+partidaa@users.noreply.github.com>
2018-09-18 20:49:42 -0500

    Add files via upload
    before parsing

--- books/books1.py
+++ books/books1.py
@@ -1,28 +1,39 @@
-#Addison Partida, Javin White, Kellen Dorchen
-#September 18, 2018
-#Reads a csv file and sorts according to command line argument.
 import csv
 import sys
 
-with open(sys.argv[1], 'r', encoding='utf-8') as csv_file:
+
+with open("books.csv", 'r', encoding='utf-8') as csv_file:
     csv_reader = csv.reader(csv_file)
-
-
-    if sys.argv[2] == "books":
-        books = []
+    
+    if sys.argv[1] == "books":
+        books= []
         for line in csv_reader:
             books.append(line[0])
-        books.sort()
-        for i in books:
-            print(i)
-
-    if sys.argv[2] == "authors":
-        authors = []
+        
+        if sys.argv[2] == "forward":
+            books.sort()
+            for i in books:
+                print (i)
+            
+        if sys.argv[2] == "reverse":
+            books.sort(reverse = True)
+            for i in books:
+                print (i)
+                
+            
+    if sys.argv[1] == "authors":
+        authors=[]
         for line in csv_reader:
             authors.append(line[2])
-        authors.sort()
-        for i in authors:
-            print(i)
+        
+        if sys.argv[2] == "forward":
+            #print (authors)
 
-    else:
-        print("Usage: python3 books1.py input-file action [sort direction]", file=sys.stderr)
+            for i in authors:
+                print (i)
+            
+        if sys.argv[2] == "reverse":
+            authors.sort(reverse = True)
+            for i in authors:
+                print (i)
+        
