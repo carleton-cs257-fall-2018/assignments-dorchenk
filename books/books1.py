2c52183af8fb249414a1441eed5cdb4b61626529
Javin White <javin@eduroam-164-88.dyn.carleton.edu>
2018-09-18 20:29:55 -0500

    command line

--- books/books1.py
+++ books/books1.py
@@ -1,18 +1,18 @@
 import csv
 import sys
 
-with open('books.csv', 'r', encoding='utf-8') as csv_file:
+with open(sys.argv[1], 'rb') as csv_file:
     csv_reader = csv.reader(csv_file)
    
     
-    if sys.argv[1] == "books":
+    if sys.argv[2] == "books":
         books= []
         for line in csv_reader:
             books.append(line[0])
         books.sort()
         print (books)
     
-    if sys.argv[1] == "authors":
+    if sys.argv[2] == "authors":
         authors=[]
         for line in csv_reader:
             authors.append(line[2])
