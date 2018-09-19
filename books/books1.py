56a521869f47e075377f0831afdb3591f39766a3
adoseofkellen <32659062+adoseofkellen@users.noreply.github.com>
2018-09-18 21:14:23 -0500

    Details added

--- books/books1.py
+++ books/books1.py
@@ -9,20 +9,36 @@ with open(sys.argv[1], 'r', encoding='utf-8') as csv_file:
 
 
     if sys.argv[2] == "books":
-        books = []
+        books= []
         for line in csv_reader:
             books.append(line[0])
-        books.sort()
-        for i in books:
-            print(i)
+
+        if sys.argv[3] == "forward":
+            books.sort()
+            for i in books:
+                print (i)
+
+        elif sys.argv[3] == "reverse":
+            books.sort(reverse = True)
+            for i in books:
+                print (i)
+
 
     elif sys.argv[2] == "authors":
-        authors = []
+        authors=[]
         for line in csv_reader:
             authors.append(line[2])
-        authors.sort()
-        for i in authors:
-            print(i)
+
+        if sys.argv[3] == "forward":
+            authors.sort()
+            #print (authors)
+            for i in authors:
+                print (i)
+
+        elif sys.argv[3] == "reverse":
+            authors.sort(reverse = True)
+            for i in authors:
+                print (i)
 
     else:
         print("Usage: python3 books1.py input-file action [sort direction]", file=sys.stderr)
