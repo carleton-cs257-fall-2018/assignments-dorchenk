ac447557071095002f7b6b3bf2b71f1af4667460
partidaa <36749876+partidaa@users.noreply.github.com>
2018-09-18 21:47:30 -0500

    Add files via upload
    with dates still

--- books/books1.py
+++ books/books1.py
@@ -1,44 +1,39 @@
-#Addison Partida, Javin White, Kellen Dorchen
-#September 18, 2018
-#Reads a csv file and sorts according to command line argument.
 import csv
 import sys
 
-with open(sys.argv[1], 'r', encoding='utf-8') as csv_file:
+
+with open("books.csv", 'r', encoding='utf-8') as csv_file:
     csv_reader = csv.reader(csv_file)
-
-
-    if sys.argv[2] == "books":
+    
+    if sys.argv[1] == "books":
         books= []
         for line in csv_reader:
             books.append(line[0])
-
-        if sys.argv[3] == "forward":
+        
+        if sys.argv[2] == "forward":
             books.sort()
             for i in books:
                 print (i)
-
-        elif sys.argv[3] == "reverse":
+            
+        if sys.argv[2] == "reverse":
             books.sort(reverse = True)
             for i in books:
                 print (i)
-
-
-    elif sys.argv[2] == "authors":
+                
+            
+    if sys.argv[1] == "authors":
         authors=[]
         for line in csv_reader:
             authors.append(line[2])
-
-        if sys.argv[3] == "forward":
-            authors.sort()
-            #print (authors)
+        
+        if sys.argv[2] == "forward":
+            
+            authors = sorted(sorted(authors), key=lambda n: n.split()[1])
             for i in authors:
                 print (i)
-
-        elif sys.argv[3] == "reverse":
+            
+        if sys.argv[2] == "reverse":
             authors.sort(reverse = True)
+            authors = sorted(sorted(authors), key=lambda n: n.split()[1], reverse = True)
             for i in authors:
-                print (i)
-
-    else:
-        print("Usage: python3 books1.py input-file action [sort direction]", file=sys.stderr)
+                print(i)
