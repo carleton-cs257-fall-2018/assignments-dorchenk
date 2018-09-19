8a3cb42e3c3dc1c2bcb7d79e96ad9092cb3f0789
partidaa <36749876+partidaa@users.noreply.github.com>
2018-09-18 19:38:06 -0500

    Add files via upload

--- books/books1.py
+++ books/books1.py
@@ -1,23 +1,11 @@
-import csv
-import sys
-import pandas as pd
-with open('books.csv', newline='') as f:
-    reader = csv.reader(f)
-    data=list(reader)
-    print(data)
-if sys.argv[1] == "author":
-    books = pandas.read_csv('books.csv', data[0])
-    sortedB = books.sort()
-    print: ("Here are the set of all books, listed alphabetically:", sortedB)
-if sys.argv[1]:
-    author = pandas.read_csv('books.csv', data [1])
-    sortedA = authors.sort()
-    print: ("Here are the set of all authors, listed alphabetically:", sortedA)
-        
-        
- ## if user choses Author, sort and retun alphabetical array by author name
-##if sys.argv[1] == 'Author':
+import csv 
 
-    
-## if user choses book, sort and retun alphabetical array by book title
-    
+with open('books.csv', 'r', encoding='utf-8') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    authors =[]
+    for line in csv_reader:
+        authors.append(line[0])
+       # print(line[0])
+   # print (authors)
+    authors.sort()
+    print (authors)
