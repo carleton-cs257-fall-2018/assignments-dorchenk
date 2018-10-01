97a56af26475ad45e26650f83c8a84926034645d
Javin White <whitej2@carleton.edu>
2018-09-30 19:41:32 -0500

    Here's our file

---
+++ using-apis/api-test.py
@@ -0,0 +1,30 @@
+'''
+By Kellen Dorchen and Javin White
+September 30, 2018
+api-test.py
+'''
+
+import sys
+import argparse
+import json
+import urllib.request
+
+class Testing_APIs:
+	
+	def get_list():
+	pass
+	
+	def get_details():
+	pass
+	
+	
+# 	def main():
+# 	
+# 	
+# 	
+# 	
+# 	if __name__ == '__main__':
+# 	
+# 	
+# 	
+# 	main()
