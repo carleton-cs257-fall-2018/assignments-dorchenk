11d41afecf52bb409a2f4890f7e1e1e46dedf070
Javin White <whitej2@carleton.edu>
2018-10-15 22:55:49 -0500

    updated tests

--- webapp/tests/api_tests.py
+++ webapp/tests/api_tests.py
@@ -2,65 +2,80 @@
     api_tests.py
     Kellen Dorchen
     Javin White
-    October 12, 2018
+    October 15, 2018
 '''
 
 import unittest, json
 
 class ApiTester(unittest.TestCase):
     def setUp(self):
-        pass
+        self.data_source = people.People("data_test.csv")
 
     def tearDown(self):
         pass
 #---------------------------------------------------
     def test_person(self):
-        pass
+        self.assertEqual(self.data_source.person(name='Alexander Long'))
 
     def test_get_name(self):
-        pass
+        self.assertEqual(self.data_source.person(name='Alexander Long'))
 
     def test_get_age(self):
-        pass
+        self.assertEqual(self.data_source.age(age='23'))
+        
+    def test_age_value(self):
+    	self.assertRaises(ValueError, self.data_source.age(age='mel$3$@'))
 
     def test_get_gender(self):
-        pass
+        self.assertEqual(self.data_source.person(gender='Male'))
 
     def test_get_raceethnicity(self):
-        pass
+        self.assertEqual(self.data_source.raceethnicity(raceethnicity='Black'))
+        
+    def test_value_raceethnicity(self):
+    	self.assertRaises(self.data_source.raceethnicity(raceethnicity=55)
 
     def test_get_month(self):
-        pass
+        self.assertEqual(self.data_source.date(month='February'))
+        
+    def test_value_month(self):
+    	self.assertRaises(ValueError, self.data_source.date(month='ca-ching'))
 
     def test_get_day(self):
-        pass
+        self.assertEqual(self.data_source.date(day=25))
+        
+    def test_value_data(self):
+    	self.assertRaises(ValueError, self.data_source.date(day='Fat Tuesday'))
 
     def test_get_year(self):
-        pass
+        self.assertEqual(self.data_source.date(year=2015))
 
     def test_get_streetaddress(self):
-        pass
+        self.assertEqual(self.data_source.person(street_address='500 North Oliver Ave'))
 
     def test_get_city(self):
-        pass
+        self.assertEqual(self.data_source.city(city='Milwaukee'))
 
     def test_get_state(self):
-        pass
+        self.assertEqual(self.data_source.state(state='KS'))
+
+	def test_value_state(self):
+		self.assertRaises(ValueError, self.data_source.state(state=57))
 
     def test_get_latitude(self):
-        pass
+       	self.assertEqual(self.data_source.state(latitude='39.4629302'))
 
     def test_get_longitude(self):
-        pass
+        self.assertEqual(self.data_soure.state(longitude='-87.3788602'))
 
     def test_get_lawenforecementagency(self):
-        pass
+        self.assertEqual(self.data_source.person(law_enforcement_agency='Terre Haute Police Department'))
 
     def test_get_cause(self):
-        pass
+        self.assertEqual(self.data_source.person(cause='Gunshot'))
 
     def test_get_armed(self):
-        pass
+        self.assertEqual(self.data_source.person(armed='Knife'))
 
 #------------------------------------------------------
 if __name__ == '__main__':
