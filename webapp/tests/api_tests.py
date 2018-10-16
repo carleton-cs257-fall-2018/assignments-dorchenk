4812c86510f6d63e0aac4c397a12982722cdb586
adoseofkellen <32659062+adoseofkellen@users.noreply.github.com>
2018-10-15 21:57:59 -0500

    Updated unittests and added data to test

---
+++ webapp/tests/api_tests.py
@@ -0,0 +1,67 @@
+'''
+    api_tests.py
+    Kellen Dorchen
+    Javin White
+    October 12, 2018
+'''
+
+import unittest, json
+
+class ApiTester(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+#---------------------------------------------------
+    def test_person(self):
+        pass
+
+    def test_get_name(self):
+        pass
+
+    def test_get_age(self):
+        pass
+
+    def test_get_gender(self):
+        pass
+
+    def test_get_raceethnicity(self):
+        pass
+
+    def test_get_month(self):
+        pass
+
+    def test_get_day(self):
+        pass
+
+    def test_get_year(self):
+        pass
+
+    def test_get_streetaddress(self):
+        pass
+
+    def test_get_city(self):
+        pass
+
+    def test_get_state(self):
+        pass
+
+    def test_get_latitude(self):
+        pass
+
+    def test_get_longitude(self):
+        pass
+
+    def test_get_lawenforecementagency(self):
+        pass
+
+    def test_get_cause(self):
+        pass
+
+    def test_get_armed(self):
+        pass
+
+#------------------------------------------------------
+if __name__ == '__main__':
+    unittest.main()
