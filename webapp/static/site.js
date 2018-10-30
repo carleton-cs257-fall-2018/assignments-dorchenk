22ace65a90d9c4c13243a2ef1419f281330e8ccb
Javin White <whitej2@carleton.edu>
2018-10-29 19:11:28 -0500

    first draft of site.js

--- webapp/static/site.js
+++ webapp/static/site.js
@@ -1,1 +1,47 @@
-site.js
+//site.js
+
+initialize();
+
+function initialize() {
+	
+}
+
+function getBaseURL() {
+	var baseURL = window.location.protocol + '//' + window.location.hostname + ':' + api_port;
+	return baseURL;
+}
+
+function onHomeTabClicked() {
+	var url = getBaseURL();
+
+}
+
+function onNameTabClicked() {
+	var url = getBaseURL() + '/name';
+}
+
+function onAgeTabClicked() {
+	var url = getBaseURL() + '/age';
+
+}
+
+function onRaceTabClicked() {
+	var url = getBaseURL() + '/race';
+
+}
+
+function onDateTabClicked() {
+	var url = getBaseURL() + '/date';
+}
+
+function onCityTabClicked() {
+	var url = getBaseURL() + '/city';
+}
+
+function onStateTabClicked() {
+	var url = getBaseURL() + '/state';
+}
+
+function onDonationsTabClicked() {
+	var url = getBaseURL() + '/donations';
+}
